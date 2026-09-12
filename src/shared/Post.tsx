26aa521f1408b7bd9bf1f9c0d8d3e566c8084c62
LivingstoneDoc <livingstone.doc@yandex.ru>
2026-09-12 14:39:24 +0400

    refactor: delete extra imports

--- src/shared/Post.tsx
+++ src/shared/Post.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { ThumbsUp, MessageCircle, Share2, Send } from "lucide-react";
 import type { PostProps } from "../types/post-types";
 
